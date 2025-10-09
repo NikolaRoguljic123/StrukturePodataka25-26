d81e0d38ff7aeb8e463d876fc6925a0c3c512f36
git_NikolaRoguljic123 <ljerko01@gmail.com>
2025-10-09 20:01:16 +0200

    izmjena

---
+++ zad1/Source.c
@@ -0,0 +1,28 @@
+#define CRT_SECURE_NO_WARNING
+#define ERROR_NOT_OPENED
+#include <stdio.h>
+
+typedef struct {
+
+
+	char ime;
+	char prezime;
+	int brojbodova;
+
+}osoba;
+
+
+
+
+int brojstud() {
+
+	FILE* fp;
+	int brojac = 0;
+	fp = fopen("student.txt", "r");
+
+	while (feof(fp) == 0) {
+
+		brojac++;
+		}
+	print("%d", brojac);
+}
